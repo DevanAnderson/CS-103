d02e3e5ee8c0d9b0f41e9f2cad568fed6fc44345
DevanAnderson <devan.odin.amndersson@gmail.com>
2023-06-28 14:44:23 -0700

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//test code
+
+int main(){
+    string a;
+    cin >> a;
+    cout << "Hello World!" << endl;
+    cout << a << endl;
+}
